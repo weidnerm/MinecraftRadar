7efa9ce92e241751f46d5fe90f451091ced2ffe8
Michael Weidner <michael.weidner@motorolasolutions.com>
2019-09-08 14:48:20 -0400

    added basic block

---
+++ src/main/java/com/example/examplemod/ModBlocks.java
@@ -0,0 +1,11 @@
+package com.example.examplemod;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+public class ModBlocks {
+
+	@ObjectHolder("examplemod:firstblock")
+	public static FirstBlock FIRSTBLOCK;
+
+
+}
