89f6ab3dde9938b615e4a52cfaa2ec8259b905c7
Michael Weidner <michael.weidner@motorolasolutions.com>
2019-09-10 20:48:47 -0400

    support horizontal scans

--- src/main/java/com/example/examplemod/FirstBlock.java
+++ src/main/java/com/example/examplemod/FirstBlock.java
@@ -56,15 +56,12 @@ public class FirstBlock extends Block {
             int z = pos.getZ();
             Direction side = result.getFace();
             LOGGER.info("direction="+side );
-            
-            
-            LOGGER.info("pos = " + x + ","+ y + "," + z );
-            
         	int dirtCount = 0;
         	numGuiLayers = 0;
         	BlockPos myPos;
         	int radius = 0; //0 for 1x1;  1 for 3x3;  2 for 5x5;  3 for 7x7
-        	
+
+        	// Count adjacent radar blocks to determine radius of probe
         	if( world.getBlockState(new BlockPos(x,y+1,z)).getBlock().getNameTextComponent().getString().equals("First Block")) {radius++;}
         	if( world.getBlockState(new BlockPos(x,y-1,z)).getBlock().getNameTextComponent().getString().equals("First Block")) {radius++;}
         	if( world.getBlockState(new BlockPos(x-1,y,z)).getBlock().getNameTextComponent().getString().equals("First Block")) {radius++;}
@@ -72,38 +69,65 @@ public class FirstBlock extends Block {
         	if( world.getBlockState(new BlockPos(x,y,z-1)).getBlock().getNameTextComponent().getString().equals("First Block")) {radius++;}
         	if( world.getBlockState(new BlockPos(x,y,z+1)).getBlock().getNameTextComponent().getString().equals("First Block")) {radius++;}
 
-        	int zOffset, xOffset;
-            for(int depth=1; depth<y-1; depth++)
+            int xRadius = 0;
+            int yRadius = 0; 
+            int zRadius = 0;
+            int depthMax = 0;
+            
+            if      (side == Direction.UP)    { xRadius=radius; yRadius=0;      zRadius=radius; depthMax = y-1; } 
+            else if (side == Direction.DOWN)  { xRadius=radius; yRadius=0;      zRadius=radius; depthMax = 32; }
+            else if (side == Direction.EAST)  { xRadius=0;      yRadius=radius; zRadius=radius; depthMax = 32; }
+            else if (side == Direction.WEST)  { xRadius=0;      yRadius=radius; zRadius=radius; depthMax = 32; }
+            else if (side == Direction.NORTH) { xRadius=radius; yRadius=radius; zRadius=0;      depthMax = 32; }
+            else if (side == Direction.SOUTH) { xRadius=radius; yRadius=radius; zRadius=0;      depthMax = 32; }
+            
+            int diameter = 1+radius*2;
+            splashText = new String(diameter+"x"+diameter+"x"+depthMax+" Scanned");
+            LOGGER.info("pos = " + x + ","+ y + "," + z + " for range of " + depthMax + " blocks " + splashText );
+        	
+        	int zOffset, xOffset, yOffset;
+            for(int depth=1; depth<depthMax; depth++)
             {
-            	int newY = y-depth;
-            	
-                for(xOffset=-radius; xOffset<1+radius ; xOffset++ )
+                for(xOffset=-xRadius; xOffset<1+xRadius ; xOffset++ )
                 {
-                   for(zOffset=-radius; zOffset<1+radius ; zOffset++ )
-                   {
-		            	int newX = x+xOffset;
-		            	int newZ = z+zOffset;
+                    for(zOffset=-zRadius; zOffset<1+zRadius ; zOffset++ )
+                    {
+                        for(yOffset=-yRadius; yOffset<1+yRadius ; yOffset++ )
+                        {
+			            	int newX = x+xOffset;
+			            	int newZ = z+zOffset;
+			            	int newY = y+yOffset;
+			            	
+			                if      (side == Direction.UP)    { newY -= depth; }
+			                else if (side == Direction.DOWN)  { newY += depth; }
+			                else if (side == Direction.EAST)  { newX -= depth; }
+			                else if (side == Direction.WEST)  { newX += depth; }
+			                else if (side == Direction.NORTH) { newZ += depth; }
+			                else if (side == Direction.SOUTH) { newZ -= depth; }
 
-		            	myPos = new BlockPos(newX,newY,newZ);
-		            	BlockState myBlockState = world.getBlockState(myPos);
-		            	String blockName = myBlockState.getBlock().getNameTextComponent().getString();
-            	
-		            	for(int typeIndex=0; typeIndex<itemNames.length ; typeIndex++)
-		            	{
-			            	if (blockName.equals(itemNames[typeIndex]))
+			                if (newY < 0) { newY = 0; }
+			            	
+			            	myPos = new BlockPos(newX,newY,newZ);
+			            	BlockState myBlockState = world.getBlockState(myPos);
+			            	String blockName = myBlockState.getBlock().getNameTextComponent().getString();
+	            	
+			            	for(int typeIndex=0; typeIndex<itemNames.length ; typeIndex++)
 			            	{
-			            		if(numFound[typeIndex] == 0)
-			            		{
-			            			if(numGuiLayers<maxGuiLayers)
-			            			{
-				            			sortedItemDepth[typeIndex] = y-newY;
-			            				layerItemIndexes[numGuiLayers] = typeIndex;
-			            				numGuiLayers++;
-			            			}
-			            		}
-			            		numFound[typeIndex]++;
-			            	}
-						}
+				            	if (blockName.equals(itemNames[typeIndex]))
+				            	{
+				            		if(numFound[typeIndex] == 0)
+				            		{
+				            			if(numGuiLayers<maxGuiLayers)
+				            			{
+					            			sortedItemDepth[typeIndex] = depth;
+				            				layerItemIndexes[numGuiLayers] = typeIndex;
+				            				numGuiLayers++;
+				            			}
+				            		}
+				            		numFound[typeIndex]++;
+				            	}
+							}
+                        }
 					}
 				}
 			}
@@ -115,8 +139,6 @@ public class FirstBlock extends Block {
 	            LOGGER.info(itemNames[itemIndex]+" count="+numFound[itemIndex]+" firstY="+sortedItemDepth[itemIndex]);
             }
             
-            int diameter = 1+radius*2;
-            splashText = new String(diameter+"x"+diameter+" Scanned");
 
             displayDuration = 1500;
             
